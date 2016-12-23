0d1caa8a161fb3e967f39b98d0e5dbf21bd4f080
user <user@kosta00>
2016-12-23 18:47:01 +0900

    게임주석

--- src/member/dao/AdminGameDao.java
+++ src/member/dao/AdminGameDao.java
@@ -5,13 +5,21 @@ import java.util.List;
 import member.vo.GameVO;
 
 public interface AdminGameDao {
-//	List<GameVO> GameBoard();
+	
+//	관리자에서 게임 입력
 	int adminGameInput(GameVO vo);
+//게임실행창을 띄우기 위한 값 가져오기
 	GameVO GameView(GameVO vo);
+//	관리자에서 게임 삭제
 	int adminGameDelete(GameVO vo);
+//	관리자에서 게임 수정
 	int adminGameUpdate(GameVO vo);
+//게임 추천을 위한 추천인 리스트 가져오기
 	List<GameVO> GameRecom(GameVO vo);
+//	게임 추천 실행
 	int GameRecomDoing(GameVO vo);
+//	페이징처리된 게임 리스트 가져오기
 	List<GameVO> GameBoard(GameVO vo);
+//	페이징을 위한 총 게임 갯수 가져오기
 	int gameListCount();
 }
--- src/mybatis/manager/GameManager.java
+++ src/mybatis/manager/GameManager.java
@@ -17,51 +17,48 @@ import member.vo.MemberVO;
 public class GameManager {
 
 	 
+	//게임 입력 실행
 	public static int gameInput(GameVO vo ) throws SQLException
 	   {
 			int result = 0;
 		   SqlSession session = CommonManager.db().openSession();
 		   result = session.insert("admin.gameInput", vo);
 		   session.commit();
-		   	return result;	// insert()�� ����Ÿ���� Object�̱⵵ �ϰ� update()�� ����ص� ����
+		   	return result;
 	   }
 	
+//	페이징을 위한 총 게임 갯수 가져오기
 	public static int totalCount() throws SQLException {
 	      SqlSession session = CommonManager.db().openSession();
 	      int totalCount = session.selectOne("admin.gameTotalCount");
-
 	      session.commit();
 	      return totalCount;
 	   }
 	
+//	페이징처리가 된 게임 리스트 가져오기
 	public static List<GameVO> gameBoard(GameVO vo) throws SQLException
 	   {
 			SqlSession session = CommonManager.db().openSession();
 			List<GameVO> list=null;
-			
 				list = session.selectList("admin.gameAllp",vo);
-				System.out.println(list.size());
-		   
 		   session.commit();
 		   	return list;	
 	   }
 	
-
-	
+//게임 뷰(게임실행 화면)을 띄우기 위해 해당 값 가져오기
 	public static GameVO gameView(GameVO vo) throws SQLException
 	   {
 			SqlSession session = CommonManager.db().openSession();
 			GameVO vlist=null;
-			
+			//게임 조회수 1증가
 			session.update("admin.gameCount", vo);
-				vlist = session.selectOne("admin.gameAll", vo);
-				System.out.println(vlist);
-		   
+				vlist = session.selectOne("admin.gameAll", vo);		   
 		   session.commit();
 		   	return vlist;	
 	   }
 
-	
+
+//	게임 삭제 실행
 	public static int gameDelete(GameVO vo ) throws SQLException
 	   {
 			int result = 0;
@@ -72,6 +69,7 @@ public class GameManager {
 		   	
 	   }
 	
+//	게임 수정 실행
 	public static int gameUpdate(GameVO vo ) throws SQLException
 	   {
 			int result = 0;		
@@ -81,18 +79,17 @@ public class GameManager {
 		   	return result;		   
 	   }
 	
+//	게임 추천을 위한 추천자 리스트 가져오기
 	public static List<GameVO> gameRecom(GameVO vo) throws SQLException
 	   {
 			SqlSession session = CommonManager.db().openSession();
 			List<GameVO> list=null;
-			
 				list = session.selectList("admin.gameRecom", vo);
-				System.out.println(list.size());
-		   
 		   session.commit();
 		   	return list;	
 	   }
-	
+
+//	게임 추천실행
 	public static int gameRecomDoing(GameVO vo ) throws SQLException
 	   {
 			int result = 0;		
